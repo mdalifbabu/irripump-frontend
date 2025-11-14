89ffd20ecf4d58801445ce784e365b3c25d64e9d
MD ALIF BABU <alif@k53tech.com>
2025-11-15 03:09:44 +0600

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
